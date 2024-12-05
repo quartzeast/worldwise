ec5dd861cdc0fedef6bf686eed7bff4b1b3be25d
shiyingdong <shiyingdong@webull.com>
2024-12-05 11:14:19 +0800

    Using CSS Modules

---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,13 @@
+import AppNav from '../components/AppNav'
+
+function AppLayout() {
+  return (
+    <div>
+      <AppNav />
+
+      <h1>App</h1>
+    </div>
+  )
+}
+
+export default AppLayout
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,14 +1,16 @@
 import { NavLink } from 'react-router'
 import PageNav from '../components/PageNav'
+import AppNav from '../components/AppNav'
 
 function Homepage() {
   return (
     <div>
       <PageNav />
+      <AppNav />
 
-      <h1>Worldwise</h1>
+      <h1 className="test">Worldwise</h1>
 
-      <NavLink to="/pricing">Pricing</NavLink>
+      <NavLink to="/app">Go to app</NavLink>
     </div>
   )
 }
